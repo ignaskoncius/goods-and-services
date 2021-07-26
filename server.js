67abf187aaf5bc0f5efc45e04f8ae102f56e771d
Ignas Koncius <i.koncius@gmail.com>
2021-07-26 09:18:37 +0300

    i dotenv, npm start script, connect to mongoDb

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const app = express();
 
+const PORT = 3000;
+
 // Middleware
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
 
-app.listen(3000);
+mongoose
+  .connect(process.env.MONGO_CONN_STRING, {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => {
+    console.log('Connected to Mongoooooose');
+  })
+  .catch((err) => console.log(err));
+
+app.listen(PORT, console.log(`Server is running on port ${PORT}`));
