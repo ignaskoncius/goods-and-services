c9481617f8acc8420e0e260a049f06110cfce76b
Ignas Koncius <i.koncius@gmail.com>
2021-07-26 14:40:38 +0300

    filter + delete routes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -23,4 +23,26 @@ router.get('/allGoodsServices', async (req, res) => {
   }
 });
 
+// filter all items
+router.get('/allGoodsServices/:filterValue', async (req, res) => {
+  const filterBy = req.params.filterValue;
+  try {
+    const allGoodsServicesFromDb = await GoodsOrServices.find({ goodsServicesType: filterBy });
+    res.json(allGoodsServicesFromDb);
+  } catch (error) {
+    res.status(500).json();
+  }
+});
+
+// delete item
+router.delete('/allGoodsServices/delete/:itemId', async (req, res) => {
+  const idOfItemToDelete = req.params.itemId;
+  try {
+    const deleteResult = await GoodsOrServices.findByIdAndDelete(idOfItemToDelete);
+    res.json(deleteResult);
+  } catch (error) {
+    res.status(500).json();
+  }
+});
+
 module.exports = router;
