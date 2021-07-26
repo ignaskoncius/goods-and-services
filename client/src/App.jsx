338cf9a58665e6b5644ed6cfc9adb343f288507f
Ignas Koncius <i.koncius@gmail.com>
2021-07-26 11:31:16 +0300

    app.jsx cc

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,20 @@
+import React, { Component } from 'react';
 import './App.css';
 import MyForm from './components/MyForm';
 
-function App() {
-  return (
-    <div className="App">
-      <MyForm></MyForm>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { allData: [] };
+  }
+  state = {};
+  render() {
+    return (
+      <div className="App">
+        <MyForm></MyForm>
+      </div>
+    );
+  }
 }
 
 export default App;
