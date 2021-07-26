4aee6ce2dc355f1161414acb0c653f67d04e5963
Ignas Koncius <i.koncius@gmail.com>
2021-07-26 14:02:09 +0300

    cdm, getAllDataFromDb

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
+import { getAllData } from './utils/requests';
 import './App.css';
 import MyForm from './components/MyForm';
+import GoodsServicesList from './components/GoodsServicesList';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = { allData: [] };
   }
-  state = {};
+
+  componentDidMount() {
+    this.getAllDataFromDb();
+  }
+
+  getAllDataFromDb = async () => {
+    const allData = await getAllData();
+    this.setState({ allData: allData });
+    console.log(this.state.allData);
+  };
+
+  FilterGoodsServices = async (filterValue) => {
+    console.log('filtruoji itemus');
+  };
+
   render() {
     return (
       <div className="App">
-        <MyForm></MyForm>
+        <MyForm onGetAllDataFromDb={this.getAllDataFromDb}></MyForm>
+        <GoodsServicesList
+          onFilterGoodsServices={this.FilterGoodsServices}
+          allData={this.state.allData || []}
+        ></GoodsServicesList>
       </div>
     );
   }
